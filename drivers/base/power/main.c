71df5230c2d42e607ea3421fed553d131dd7b8b5
chrmhoffmann <chrmhoffmann@gmail.com>
2017-08-17 08:35:55 +0200

    FP2: fix mismerge - and the crash on suspend
    Change-Id: I34efaa2be2178e75905d1665a17629b93e7f60ca

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1148,6 +1148,9 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 
 	device_unlock(dev);
 
+	del_timer_sync(&timer);
+	destroy_timer_on_stack(&timer);
+
  Complete:
 	complete_all(&dev->power.completion);
 
